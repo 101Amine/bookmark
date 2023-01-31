5c8e6ad17c34d50839da9133fd53d61f0cdd7fe8
101Amine <aminebecha8@gmail.com>
2023-01-31 21:53:05 +0100

    ..

--- src/components/Introduction/index.jsx
+++ src/components/Introduction/index.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import hero from '../../assets/images/illustration-hero.svg';
 
 function Introduction() {
   return (
     <div className="mx-auto mt-[3.5625rem] flex max-w-[73.375rem] flex-col items-center justify-center px-4 md:flex-row-reverse md:justify-between md:gap-16 md:px-8">
-      <div className="width-fill-available banner-1 flex-[1.08]">
+      <div className="banner-1 width-fill-available flex-[1.08]">
         <img
           src={hero}
           alt=""
@@ -20,17 +19,17 @@ function Introduction() {
           A Simple Bookmark Manager
         </h1>
         <p className="text-very-dark-blue/50 md:text-normal">
-          A clean and simple interface to organize your favourite websites. Open a new browser tab
+          A clean and simple interface to organize your favorite websites. Open a new browser tab
           and see your sites load instantly. Try it for free.
         </p>
         <div className="mt-8 flex flex-col justify-center gap-[0.75rem] md:my-[0.5625rem] md:flex-row md:justify-start">
           <a
-            href="#"
+            href="/#"
             className="rounded-md border-[0.188rem] border-soft-blue bg-soft-blue py-[0.655rem] px-[1.521rem] text-[0.875rem] font-bold text-white transition duration-300 hover:bg-white hover:text-soft-blue">
             Get it on Chrome
           </a>
           <a
-            href="#"
+            href="/#"
             className="rounded-md border-[0.188rem] border-white bg-white py-[0.655rem] px-[1.521rem] text-[0.875rem] font-bold text-gray-700 shadow-2xl transition duration-300 hover:border-very-dark-blue">
             Get it on Firefox
           </a>
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HamburgerMenu from './Hamburger';
 
 function Navbar() {
+  const [isMenuOpen, setOpenMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOpenMenu(isMenuOpen);
+    document.body.classList.toggle('lock-scroll');
+  };
   return (
     <nav aria-label="Main">
+      <HamburgerMenu toggleMenu={toggleMenu} isMenuOpen={isMenuOpen} />
       <ul
         id="navbar-menu"
-        className="gap-4 transition-visibility invisible absolute top-0 left-0 z-40 flex h-full w-full flex-col items-center justify-between bg-very-dark-blue/90 tracking-widest opacity-0 md:visible md:static md:h-auto md:w-auto md:flex-row md:items-center md:justify-start md:gap-[3rem] md:bg-transparent md:p-0 md:text-[0.8125rem] md:opacity-100">
+        className={`transition-visibility invisible absolute top-0 left-0 z-40 flex h-full w-full flex-col items-center justify-between bg-very-dark-blue/90 tracking-widest opacity-0 md:visible md:static md:h-auto md:w-auto md:flex-row md:items-center md:justify-start md:gap-[3rem] md:bg-transparent md:p-0 md:text-[0.8125rem] ${
+          isMenuOpen ? '!visible px-8  pt-24 pb-8 opacity-100' : ''
+        }`}>
         <li className="uppercase tracking-wider hover:text-soft-red">
           {' '}
           <a href="/#">features</a>
